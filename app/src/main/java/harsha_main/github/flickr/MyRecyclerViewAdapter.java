c7d83ad85a9f7b840b6c92efa4b97bf1141b375a
Harsha <9515246+harsha-main@users.noreply.github.com>
2018-06-28 22:36:45 +0530

    Text formatting improved
    Action bar is replaced with a Textview

--- app/src/main/java/harsha_main/github/flickr/MyRecyclerViewAdapter.java
+++ app/src/main/java/harsha_main/github/flickr/MyRecyclerViewAdapter.java
@@ -3,13 +3,13 @@ package harsha_main.github.flickr;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +27,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     private Item[] mData;
     private LayoutInflater mInflater;
     private boolean expanded[];
+
     MyRecyclerViewAdapter(Context context, Item[] data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
@@ -48,10 +49,10 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         Glide.with(c).load("https://farm" + mData[position * 2].farmid + ".staticflickr.com/" + mData[position * 2].serverid + "/" + mData[position * 2].imageid + "_" + mData[position * 2].secret + "_m.jpg")
                 .into(holder.image1);
         Glide.with(c).load("https://farm" + mData[position * 2 + 1].farmid + ".staticflickr.com/" + mData[position * 2 + 1].serverid + "/" + mData[position * 2 + 1].imageid + "_" + mData[position * 2 + 1].secret + "_m.jpg")
-               .into(holder.image2);
+                .into(holder.image2);
 
 
-        holder.text.setVisibility(GONE);
+        holder.linearLayout.setVisibility(GONE);
         holder.image1.clearColorFilter();
         holder.image2.clearColorFilter();
         holder.image1.setOnClickListener(new View.OnClickListener() {
@@ -60,12 +61,12 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
                 holder.image2.clearColorFilter();
                 expanded[position] = !expanded[position];
-                if(!expanded[position])
+                if (!expanded[position])
                     holder.image1.clearColorFilter();
                 else
-                holder.image1.setColorFilter(Color.argb(100, 0, 0, 255));
+                    holder.image1.setColorFilter(Color.argb(100, 0, 0, 255));
 
-                change(holder, position,holder.str1,1);
+                change(holder, position, holder.str1, 1);
             }
         });
         holder.image2.setOnClickListener(new View.OnClickListener() {
@@ -74,11 +75,11 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
                 holder.image1.clearColorFilter();
                 expanded[position] = !expanded[position];
-                if(!expanded[position])
+                if (!expanded[position])
                     holder.image2.clearColorFilter();
                 else
                     holder.image2.setColorFilter(Color.argb(100, 0, 0, 255));
-                change(holder, position,holder.str2,2);
+                change(holder, position, holder.str2, 2);
             }
         });
 
@@ -89,35 +90,48 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
         holder.text.setText("Loading...");
         if (expanded[position])
-            holder.text.setVisibility(View.VISIBLE);
+            holder.linearLayout.setVisibility(View.VISIBLE);
         else
-            holder.text.setVisibility(GONE);
-        int add=0;
-        if(k==2){add=1;}
-        StringRequest request = new StringRequest("https://api.flickr.com/services/rest/?&method=flickr.photos.getInfo&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&photo_id="+mData[position*2+add].imageid, new Response.Listener<String>() {
+            holder.linearLayout.setVisibility(GONE);
+        int add = 0;
+        if (k == 2) {
+            add = 1;
+            holder.tri.setImageResource(R.drawable.right);
+        } else holder.tri.setImageResource(R.drawable.left);
+        StringRequest request = new StringRequest("https://api.flickr.com/services/rest/?&method=flickr.photos.getInfo&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&photo_id=" + mData[position * 2 + add].imageid, new Response.Listener<String>() {
             public void onResponse(String response) {
-                String string="";
-                String s[]=response.split("realname");
-                if(s.length<2){error();return;}
-                String info[]=s[1].split("\"");
-                String t[]=response.split("<title>");
-                if(t.length<2){error();return;}
-                String title=t[1].split("<")[0];
-                string+=title;
-                if(info.length<4){error();return;}
-                string+="\n Name: "+info[1] + "\n Location: "+info[3];
-                string += "\nDate taken: "+response.split("taken=")[1].split("\"")[1];
-               holder.text.setText(string);
+                String string = "";
+                String s[] = response.split("realname");
+                if (s.length < 2) {
+                    error();
+                    return;
+                }
+                String info[] = s[1].split("\"");
+                String t[] = response.split("<title>");
+                if (t.length < 2) {
+                    error();
+                    return;
+                }
+                String title = t[1].split("<")[0];
+                string += "<b>"+title+"</b> <br />";
+                if (info.length < 4) {
+                    error();
+                    return;
+                }
+                string += "  Name: <u>" + info[1] + "</u>\n Location: " + info[3];
+                string += " <br />Date taken: " + response.split("taken=")[1].split("\"")[1];
+                holder.text.setText(Html.fromHtml(string));
             }
-            void error(){
-                Toast.makeText(c,"API Error, unable to fetch details",Toast.LENGTH_SHORT).show();
+
+            void error() {
+                Toast.makeText(c, "API Error, unable to fetch details", Toast.LENGTH_SHORT).show();
             }
         },
 
                 new Response.ErrorListener() {
                     public void onErrorResponse(VolleyError error) {
 
-                        Toast.makeText(c,"Some Error Occurred, unable to fetch details",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(c, "Some Error Occurred, unable to fetch details", Toast.LENGTH_SHORT).show();
                     }
                 });
         RequestQueue queue = Volley.newRequestQueue(c);
@@ -132,11 +146,15 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView image1, image2;
+        ImageView image1, image2, tri;
+        LinearLayout linearLayout;
         TextView text;
-        String str1,str2;
+        String str1, str2;
+
         ViewHolder(View itemView) {
             super(itemView);
+            tri = itemView.findViewById(R.id.tri);
+            linearLayout = itemView.findViewById(R.id.layout);
             image2 = itemView.findViewById(R.id.image2);
             text = itemView.findViewById(R.id.text);
             image1 = itemView.findViewById(R.id.image);
